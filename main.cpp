739fabed93df7e03ec6ead308137581cdd6f54ad
mohammed <mohalali0606@gmail.com>
2025-04-09 10:49:33 +0300

    create c++ file

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define tt int t; cin>>t;while(t--)
+void solve() {
+   cout<<"Welcome c++\n";
+    cout<<" I tried with Git & GitHub";
+
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //tt
+    solve();
+    return 0;
+}
